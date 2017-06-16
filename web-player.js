408fc3311aac05c6b4e8476118c0ac4a219227c4
Alec Grant <aleclouisgrant@gmail.com>
2017-06-16 13:55:12 -0400

    Started fullscreen and audio source buttons

--- web-player.js
+++ web-player.js
@@ -3,12 +3,15 @@ document.addEventListener("DOMContentLoaded", initializeWebPlayer(), false);
 var webPlayer;
 var playButton;
 var volButton;
+var fullscreenButton;
+
 var vol = 1;
 
 function initializeWebPlayer() {
     webPlayer = document.getElementById("player-video"); 
     playButton = document.getElementById("play-button");
     volButton = document.getElementById("vol-button");
+    fullscreenButton = document.getElementById("fullscreen-button");
 
     webPlayer.controls = false;
 }
@@ -24,9 +27,15 @@ function toggleMute() {
     if (webPlayer.muted){
         webPlayer.muted = false;
         webPlayer.volume = vol;
+        volButton.innerHTML = "Mute";
     }
     else {
         webPlayer.muted = true;
         webPlayer.volume = 0;
+        volButton.innerHTML = "Unmute";
     }
+}
+
+function toggleFullscreen() {
+    
 }
