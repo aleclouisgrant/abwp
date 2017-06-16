ca1cebe0b4ab5486a723efab410e6392f75bbc3b
Alec Grant <aleclouisgrant@gmail.com>
2017-06-16 15:03:16 -0400

    Added icons

--- web-player.js
+++ web-player.js
@@ -5,6 +5,10 @@ var playButton;
 var volButton;
 var fullscreenButton;
 
+var playButtonImg;
+var volButtonImg;
+var fullscreenButtonImg;
+
 var vol = 1;
 
 function initializeWebPlayer() {
@@ -13,26 +17,38 @@ function initializeWebPlayer() {
     volButton = document.getElementById("vol-button");
     fullscreenButton = document.getElementById("fullscreen-button");
 
+    playButtonImg = document.getElementById("play-button-img");
+    volButtonImg = document.getElementById("vol-button-img");
+    fullscreenButtonImg = document.getElementById("fullscreen-button-img");
+
     webPlayer.controls = false;
 }
 
 function togglePlayPause() { 
-    if (webPlayer.paused) 
-        webPlayer.play(); 
-    else 
+    if (webPlayer.paused) {
+        webPlayer.play();
+        playButtonImg.src = "Graphics/pause.png";
+        playButton.title = "Pause";
+    }
+    else { 
         webPlayer.pause(); 
+        playButtonImg.src = "Graphics/play.png";
+        playButton.title = "Play";
+    }
 } 
 
 function toggleMute() {
     if (webPlayer.muted){
         webPlayer.muted = false;
         webPlayer.volume = vol;
-        volButton.innerHTML = "Mute";
+        volButton.title = "Mute";
+        volButtonImg.src = "Graphics/vol.png"
     }
     else {
         webPlayer.muted = true;
         webPlayer.volume = 0;
-        volButton.innerHTML = "Unmute";
+        volButton.title = "Unmute";
+        volButtonImg.src = "Graphics/muted.png"
     }
 }
 
