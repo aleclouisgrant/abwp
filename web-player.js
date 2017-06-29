9a0a49cc9139d87fcdd826597b7fe139351edb32
Alec Grant <aleclouisgrant@gmail.com>
2017-06-29 10:55:06 -0400

    Synced audio track with video

--- web-player.js
+++ web-player.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", initializeWebPlayer(), false);
 
 var webPlayer;
+var audioPlayer;
+
 var playButton;
 var volButton;
+var audioSourceButton;
 var fullscreenButton;
 
 var playButtonImg;
@@ -16,8 +19,11 @@ var vol = 1;
 
 function initializeWebPlayer() {
     webPlayer = document.getElementById("player-video");
+    audioPlayer = document.getElementById("player-audio");
+
     playButton = document.getElementById("play-button");
     volButton = document.getElementById("vol-button");
+    audioSourceButton = document.getElementById("audio-source-button");
     fullscreenButton = document.getElementById("fullscreen-button");
 
     playButtonImg = document.getElementById("play-button-img");
@@ -28,30 +34,52 @@ function initializeWebPlayer() {
 }
 
 function togglePlayPause() { 
-    if (webPlayer.paused) {
+    if (webPlayer.paused) { //play
         webPlayer.play();
         playButtonImg.src = "Graphics/pause.png";
         playButton.title = "Pause";
+
+        audioPlayer.play();
     }
-    else { 
+    else { //pause 
         webPlayer.pause(); 
         playButtonImg.src = "Graphics/play.png";
         playButton.title = "Play";
+        
+        audioPlayer.pause();
     }
 } 
 
 function toggleMute() {
-    if (webPlayer.muted){
+    if (webPlayer.muted) { //unmute
+
         webPlayer.muted = false;
         webPlayer.volume = vol;
         volButton.title = "Mute";
         volButtonImg.src = "Graphics/vol.png"
+
+        //syncs audio
+        audioPlayer.currentTime = webPlayer.currentTime;
+        audioPlayer.play();
     }
-    else {
+    else { //mute
         webPlayer.muted = true;
         webPlayer.volume = 0;
         volButton.title = "Unmute";
         volButtonImg.src = "Graphics/muted.png"
+
+        //pause audio
+        audioPlayer.pause();
+    }
+}
+
+function toggleAudio() {
+    if (audioPlayer.paused) { //unmute
+        audioPlayer.currentTime = webPlayer.currentTime;
+        audioPlayer.play();
+    } 
+    else { //mute
+        audioPlayer.pause();
     }
 }
 
